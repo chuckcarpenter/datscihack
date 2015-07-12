1465e3fbc5ce5be392d29a645f89d242056f2ca1
Ryan Mark <ryan@mrk.cc>
2015-07-12 14:47:39 -0400

    move twitter stuff into main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,27 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
 
 // load nytimes:
 $(document).ready(function() {
-  nyt('obamacare');
+
+  function mms_search(term){
+    console.log('accepted ' + term);
+    var search_url = 'http://mms-twitter-endpoint.herokuapp.com/twitter/' + encodeURI(term);
+    console.log('searching ' + search_url);
+    $.getJSON( search_url, function( data ) {
+      console.log(data.results);
+      $('.twitter_result').html('');
+
+      for(i = 0; i < data.results.length; i++){
+        console.log('rendering: ' + data.results[i]['text']);
+        $('.twitter_result').append('<li class="list-group-item"><p class="tweet-text">'+data.results[i]['text']+'</p><span class="tweet-author">'+data.results[i]['user']['']+'</span></li>');
+      }
+    });
+  }
+
+  $('#search_terms').on('keypress', _.debounce(function(){
+    var term = $('#search_terms').val();
+    mms_search(term);
+    nyt(term);
+  }, 300));
+
 });
